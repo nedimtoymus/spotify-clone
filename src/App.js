981cc0609f4ed03038e30648a7f0beda6c0244a0
nedim58 <toymusnedim.58@gmail.com>
2024-01-10 17:17:32 +0300

    The end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Login";
-import { useStateProvider } from "./utils/StateProvider";
-import { reducerCases } from "./utils/Constants";
 import Spotify from "./components/Spotify";
-
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
 export default function App() {
   const [{ token }, dispatch] = useStateProvider();
   useEffect(() => {
-    const hash = window.location.hash
-    if(hash) {
-      const token= hash.substring(1).split('&')[0].split('=')[1];
-      
-      dispatch({type: reducerCases.SET_TOKEN, token})
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if (token) {
+        dispatch({ type: reducerCases.SET_TOKEN, token });
+      }
     }
-  }, [token, dispatch]);
+    document.title = "Spotify";
+  }, [dispatch, token]);
   return <div>{token ? <Spotify /> : <Login />}</div>;
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = async () => {
-    const client_id = "b10ac7cc459d474e961a6603c15da715";
-    const redirect_uri = "https://spotify-clone-api.netlify.app/";
+    const client_id = "2a7393edbeeb4ce4a4198acb0d46877d";
+    const redirect_uri = "http://localhost:3000";
     const api_uri = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-private",
